bd682346d2eb592519e4820ebbc4b1fbbac6d00a
Abhishek Jha <jhasabhishek17@users.noreply.github.com>
2026-09-24 02:25:13 +0530

    fix the errors

---
+++ loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+//Basic syntax of for loop
+int main(){
+
+    for (int num = 1; num <=5 ; num++)
+    {
+        cout << num <<" ";
+    }
+
+    cout<<endl;
+    return 0;
+}
